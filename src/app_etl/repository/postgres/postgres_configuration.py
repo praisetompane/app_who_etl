fe4190dd97763bb0696ebdf41c7c975bfc80f824
Praise Tompane <praisetompane@icloud.com>
2025-01-28 10:42:47 +0200

    fix: use environment variables

--- src/app_etl/repository/postgres/postgres_configuration.py
+++ src/app_etl/repository/postgres/postgres_configuration.py
@@ -1,10 +1,11 @@
 class PostgresConfiguration:
-    def __init__(self, username, password, host, port, database_name) -> None:
-        self.username = username
-        self.password = password
+
+    def __init__(self, host, port, database_name, username, password) -> None:
         self.host = host
         self.port = port
         self.database_name = database_name
+        self.username = username
+        self.password = password
 
     def uri(self) -> str:
         return f"postgresql+psycopg2://{self.username}:{self.password}@{self.host}:{self.port}/{self.database_name}"
